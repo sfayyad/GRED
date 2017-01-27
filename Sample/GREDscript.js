2df008286cdccea2f91317ecbfc609f9bbc1e9a3
sfayyad <sarahfayyad19@gmail.com>
2017-01-27 09:55:11 -0500

    Update GREDscript.js

--- Sample/GREDscript.js
+++ Sample/GREDscript.js
@@ -14,28 +14,7 @@
       });
 
 
-function loadXMLDoc(theURL)
-    {
-        if (window.XMLHttpRequest)
-        {// code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
-            xmlhttp=new XMLHttpRequest();
-        }
-        else
-        {// code for IE6, IE5
-            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange=function()
-        {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                alert(xmlhttp.responseText);
-            }
-        }
-        xmlhttp.open("GET", theURL, false);
-        xmlhttp.send();
-    };
 
-    loadXMLDoc("http://greddata.greenhillsschool.org/Datalog/ENV_Hrly_2017.txt");  
 
       function drawGraphs(datastr){
       
